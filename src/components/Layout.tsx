0dcde6ae43695c419e570183946ae027d550be89
iltStudent07 <iltStudent07@labs.webagesolutions.com>
2026-07-09 23:18:07 +0000

    Fix bug caused by BrowserRouter issues

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,11 @@
 import Header from './Header'
+import type { ReactNode } from 'react'
 
-function Layout({children}) {
+interface LayoutProps {
+    children: ReactNode
+}
+
+ function Layout({children}: LayoutProps) {
     return (
         <div>
             <Header />
